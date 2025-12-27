61f810174301bd30659ee9518faf64f0b917ea9e
Tesei Roberto <roberto_tesei@yahoo.com>
2025-12-27 11:57:59 +0100

    Refactored code for the ImageGen example.

--- imageGenerator/src/main.c
+++ imageGenerator/src/main.c
@@ -6,36 +6,143 @@
 #include "mnktRenderer.h"
 
 
-int savePPMImage(const char* filename, const Framebuffer_t* fb);
+int     createResources(Framebuffer_t* fb, const size_t fbWidth, const size_t fbHeight, ShaderProgram_t* shader);
+void    destroyResources(Framebuffer_t* fb, ShaderProgram_t* shader);
+
+void    renderImage(Framebuffer_t* fb, ShaderProgram_t* shader);
+
+Vec4_t  vertexShader(const void* vertex, ShaderParameter_t* varyings, const ShaderParameter_t* uniforms);
+Vec4_t  fragmentShader(ShaderParameter_t* varyings, const ShaderParameter_t* uniforms, const Vec2_t* fragCoords, int* discard);
+
+int     savePPMImage(const char* filename, const Framebuffer_t* fb);
 
 
 int main()
 {
-        // Setup a framebuffer
+        #define FRAMEBUFFER_WIDTH 128
+        #define FRAMEBUFFER_HEIGHT 128
+
         Framebuffer_t fb;
+        ShaderProgram_t shader;
 
-        fb.width = 128;
-        fb.height = 128;
-
-        // Allocate memory for the framebuffer's color buffer
-        fb.colorBuffer = malloc( fb.width * fb.height * 3 * sizeof(unsigned char) );
-        if(fb.colorBuffer == NULL)
-        {
-                fprintf(stderr, "ERROR: failed to allocate memory for the framebuffer's color buffer!\n");
+        // Setup all resources
+        if(createResources(&fb, FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, &shader) != 0)
                 return 0;
+
+        // Render the scene and save image to file
+        renderImage(&fb, &shader);
+        savePPMImage("output.ppm", &fb);
+
+        // Deallocate all resources
+        destroyResources(&fb, &shader);
+        
+        return 0;
+}
+
+
+/**
+ * @function createResources
+ * Tries to set up the given frame buffer and shader program structs
+ * @param fb Pointer to the frame buffer struct to be set up
+ * @param fbWidth frame buffer width
+ * @param fbHeight frame buffer height
+ * @param shader Shader program to be set up
+ * @return Zero on success, non zero on failure
+*/
+int createResources(Framebuffer_t* fb, const size_t fbWidth, const size_t fbHeight, ShaderProgram_t* shader)
+{
+        // Setup frame buffer struct, if one is given 
+        if(fb != NULL)
+        {
+                // Allocate color buffer
+                fb->colorBuffer = malloc( fbWidth * fbHeight * sizeof(unsigned char) * 3 );
+                if(fb->colorBuffer == NULL)
+                {
+                        fprintf(stderr, "[ ERROR ] createResources() failed, failed to allocate memory for the frame buffer's color buffer!\n");
+                        return 1;
+                }
+
+                // Allocate depth buffer
+                fb->depthBuffer = malloc( sizeof(float) * fbWidth * fbHeight );
+                if(fb->depthBuffer == NULL)
+                {
+                        destroyResources(fb, NULL);
+
+                        fprintf(stderr, "[ ERROR ] createResources() failed, failed to allocate memory for the frame buffer's depth buffer!\n");
+                        return 1;
+                }
+
+                fb->width = fbWidth;
+                fb->height = fbWidth;
         }
 
-        // Allocate memory for the framebuffer's depth buffer
-        fb.depthBuffer = malloc( fb.width * fb.height * sizeof(float) );
-        if(fb.depthBuffer == NULL)
+        // Setup shader program struct, if one is given
+        if(shader != NULL)
         {
-                free(fb.colorBuffer);
-                fprintf(stderr, "ERROR: failed to allocate memory for the framebuffer's color buffer!\n");
-                return 0;
+                // Setup shader functions
+                shader->vertexShader = vertexShader;
+                shader->fragmentShader = fragmentShader;
+
+                // Setup the size of a single vertex (we only have 3 float value which defines the position of the vertex)
+                shader->vertexSize = sizeof(float) * 3;
         }
 
-        // Now let's render a rectangle!!!!
+        return 0;
+}
 
+
+/**
+ * @function destroyResources
+ * Deallocates memory previously allocated for the given structs
+*/
+void destroyResources(Framebuffer_t* fb, ShaderProgram_t* shader)
+{
+        // Deallocate memory for the frame buffer, if one is given 
+        if(fb != NULL)
+        {
+                if(fb->colorBuffer != NULL)
+                {
+                        free(fb->colorBuffer);
+                        fb->colorBuffer = NULL;
+                }
+
+                if(fb->depthBuffer != NULL)
+                {
+                        free(fb->depthBuffer);
+                        fb->depthBuffer = NULL;
+                }
+
+                fb->width = 0;
+                fb->height = 0;
+        }
+
+        // Deallocate memory for the shader program, if one is given 
+        if(shader != NULL)
+        {
+                shader->vertexShader = NULL;
+                shader->fragmentShader = NULL;
+
+                shader->vertexSize = 0;
+        }
+}
+
+
+/**
+ * @function renderImage
+ * Renders a scene into the given frame buffer
+ * @param fb Frame buffer in which the scene will be rendered
+ * @param shader Shader program to be used to render the scene
+*/
+void renderImage(Framebuffer_t* fb, ShaderProgram_t* shader)
+{
+        if(fb == NULL || shader == NULL)
+        {
+                fprintf(stderr, " [ ERROR ] renderImage() failed, frame buffer and/or shader is NULL!\n");
+                return;
+        }
+
+        // For test purpouses let's render a simple triangle for now
+ 
         // Define the vertices of the triangle (clockwise order)
         float vertices[] = {
                 -0.5f, -0.5f, 0.0f,
@@ -44,24 +151,44 @@ int main()
         };
 
         // Clear framebuffer content
-        mnkt_framebufferClearColor(255, 116, 0, &fb);
-        mnkt_framebufferClearDepth(0.0f, &fb);
+        mnkt_framebufferClearColor(255, 116, 0, fb);
+        mnkt_framebufferClearDepth(0.0f, fb);
 
-        mnkt_draw(vertices, sizeof(vertices) / sizeof(vertices[0]), &fb);
-
-        // Save framebuffer's content to file
-        savePPMImage("image.ppm", &fb);
-
-        // Deallocate framebuffer's memory
-        free(fb.colorBuffer);
-        free(fb.depthBuffer);
-
-        return 0;
+        mnkt_draw(vertices, 3, shader, fb);
 }
 
 
 /**
- * Saves the given image on file system as a PPM file
+ * @function vertexShader
+ * The shader program to be executed for each vertex
+ * @param vertex The vertex data to be processed
+ * @param varyings Array of values in which additional output parameters can be stored
+ * @param uniforms Uniform variables
+*/
+Vec4_t vertexShader(const void* vertex, ShaderParameter_t* variyngs, const ShaderParameter_t* uniforms)
+{
+        Vec3_t* inPos = (Vec3_t*) vertex;
+
+        return (Vec4_t) { .x = inPos->x, .y = inPos->y, .z = inPos->z, .w = 1.0f };
+}
+
+
+/**
+ * @function vertexShader
+ * The shader program to be executed for each fragment
+ * @param varyings Array of values in which the additional output parameters produced by the vertex shader are stored
+ * @param uniforms Uniform variables
+ * @param fragCoords Coordinates of the fragment to be processed, expressed in screen space
+ * @param discard Flag used to indicate that this fragment must be discared
+*/
+Vec4_t fragmentShader(ShaderParameter_t* varyings, const ShaderParameter_t* uniforms, const Vec2_t* fragCoords, int* discard)
+{
+        return (Vec4_t) { .r = 0.7f, .g = 0.25f, .b = 0.0f, .a = 1.0f };
+}
+
+
+/**
+ * Saves the content of the given frame buffer on the file system as a PPM file
  * @param filename Name to be used for the PPM image file
  * @param fb Framebuffer of which content must be saved
  * @return Zero on success, non zero on failure
@@ -72,20 +199,20 @@ int savePPMImage(const char* filename, const Framebuffer_t* fb)
 
         if(filename == NULL)
         {
-                fprintf(stderr, "ERROR: savePPMImage() failed, no file name is given!\n");
+                fprintf(stderr, "[ ERROR ] savePPMImage() failed, no file name is given!\n");
                 return -1;
         }
 
         if(fb == NULL || fb->colorBuffer == NULL)
         {
-                fprintf(stderr, "ERROR: savePPMImage() failed, no framebuffer is given!\n");
+                fprintf(stderr, "[ ERROR ] savePPMImage() failed, no framebuffer is given!\n");
                 return -1;
         }
 
         FILE* imgFile = fopen(filename, "w");
         if(imgFile == NULL)
         {
-                fprintf(stderr, "ERROR: savePPMImage() failed, file \"%s\" could not be opened!\n", filename);
+                fprintf(stderr, "[ ERROR ] savePPMImage() failed, file \"%s\" could not be opened!\n", filename);
                 return -1;
         }
 
